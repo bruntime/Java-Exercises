1290412b3e9d50815474aaad1dd053a8eadf79d9
A Valembrun <bruntime@gmail.com>
2015-06-23 22:43:43 -0400

    Intro - The Repeater (Java)
    - exercise 1 to acquaint myself with Java
    - build error:
    
    java.lang.ExceptionInInitializerError
    Caused by: java.lang.RuntimeException: Uncompilable source code - class
    S1P1 is public, should be declared in a file named S1P1.java
    at S1P1.<clinit>(repeater.java:2)
    ....
    Exception in thread "main" Java Result: 1

---
+++ TheRepeater/src/therepeater/repeater.java
@@ -0,0 +1,10 @@
+//create a class named S1P1
+public class S1P1
+{
+//this is the main method
+    public static void main (String[] args) //main method header
+    {
+    //this prints whatever is below.
+        System.out.println("Whatever you type here.... will be repeated!");
+    }
+}

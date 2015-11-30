4749a0b46aa41293b87bd6a7267524f6fcb2f289
A Valembrun <bruntime@gmail.com>
2015-11-30 14:26:56 -0500

    Exercise: Lectures 27 - 28 (Java)
    - following the Udemy course: The Complete Java Developer Course
    - exercise (Section 5 - Lectures 27 - 28), reviewing methods

---
+++ Udemy/The Complete Java Developer Course/Lecture 27/Methods/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(false, 800, 20, 5);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score + (levelCompleted * bonus);
+
+        if (gameOver) {
+            finalScore += 2000;
+            return finalScore;
+        } else {
+            return -1;
+        }
+    }
+}

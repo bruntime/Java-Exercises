012242a2f9b1f3899bed5f09f3e1d086fc8c5c47
A Valembrun <bruntime@gmail.com>
2015-12-04 22:41:08 -0500

    Exercise: Lecture 32 (Java)
    - following the Udemy course: The Complete Java Developer Course
    - (Section 6 - Lecture 32), reviewing for statement

---
+++ Udemy/The Complete Java Developer Course/Lecture 32 and Exercise/For/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 4.0));*/
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+        }
+
+        ///Using the for statement, call the calculateInterest method with
+        //the amount of 10000 with an interestRate of 2, 3, 4, 5, 6, 7, and 8
+        //and print the result to the console window
+
+        for (int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + " % interest = " + calculateInterest(10000.0, i));
+        }
+
+        System.out.println("*************************");
+        //How would you modify the for loop above to do the same thing as
+        //shown but to start from 8% and work back to 2%
+
+        for (int i = 8; i >= 2; i--) {
+            System.out.println("10,000 at " + i + " % interest = " + calculateInterest(10000.0, i));
+        }
+
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100.0));
+    }
+}
